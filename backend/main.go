add7d7388194b93fd4a079ad03cfd13049a6c54d
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2026-04-21 18:34:31 +0530

    Update error handling for .env file loading in main function

--- backend/main.go
+++ backend/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found (using env vars)")
 	}
 
 	port := os.Getenv("PORT")
